0a68c943c4b300ef8e330ddda66acde70690f62a
Kyle Coberly <kyle.coberly@gmail.com>
2019-04-04 14:50:01 -0600

    Adding routes

--- lib/controllers/authors.rb
+++ lib/controllers/authors.rb
@@ -5,5 +5,36 @@ class AuthorsController < Sinatra::Base
     @authors = Author.all
     erb :"authors/index"
   end
+
+  get "/authors/new" do
+    erb :"authors/new"
+  end
+
+  get "/authors/:id/edit" do
+    @author = Author.find(params[:id])
+    erb :"authors/edit"
+  end
+
+  post "/authors" do
+    author.create(params)
+
+    redirect "/authors"
+  end
+
+  put "/authors/:id" do
+    author = Author.find(params[:id])
+    author.update(
+      name: params[:name]
+    )
+
+    redirect "/authors"
+  end
+
+  delete "/authors/:id" do
+    author = Author.find(params[:id])
+    author.destroy
+
+    redirect "/authors"
+  end
 end
 
--- lib/controllers/books.rb
+++ lib/controllers/books.rb
@@ -1,8 +1,49 @@
 class BooksController < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views") }
+
   get "/books" do
     @books = Book.all
     erb :"books/index" 
+  end
+
+  get "/books/new" do
+    erb :"books/new"
+  end
+
+  get "/books/:id/edit" do
+    @book = Book.find(params[:id])
+    @authors = Author.all
+    erb :"books/edit"
+  end
+
+  get "/books/:id" do
+    @book = Book.find(params[:id])
+
+    erb :"books/show"
+  end
+
+  post "/books" do
+    Book.create(params)
+
+    redirect "/books"
+  end
+
+  put "/books/:id" do
+    book = Book.find(params[:id])
+    author = Author.find(params[:author_id])
+    book.update(
+      title: params[:title],
+      author: author
+    )
+
+    redirect "/books"
+  end
+
+  delete "/books/:id" do
+    book = Book.find(params[:id])
+    book.destroy
+
+    redirect "/books"
   end
 end
 
